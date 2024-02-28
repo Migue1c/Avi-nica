fadc59b225a3f6ba56b0ddd5d3b423b656ce815e
Miguel Caldeira <miguelcaldeira2002@gmail.com>
2024-02-28 17:00:39 +0000

    1

--- Ex1.py
+++ Ex1.py
@@ -1,11 +1,13 @@
 import math
 
-gamma_b = 45.01284
+gamma_b = 008.8717
+
 gg = math.floor(gamma_b)
 v = (gamma_b - gg) * 60 
 mm = math.floor(v)
 mm_f = math.modf(v)
-ss = int( mm_f * 60 )
+print(mm_f)
+ss = int( mm_f[0] * 60 )
 print(gg,'º',mm,'`',ss,'``')
 
 
